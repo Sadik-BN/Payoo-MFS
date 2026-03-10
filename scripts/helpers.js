e7002bc6b06c614fad7a4b1ef8670b0819316f05
Sadik-BN <sadikonline4@gmail.com>
2026-03-11 05:08:45 +0600

    fixed balance bug

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,6 +5,7 @@ function updateBalance() {
     document.getElementById("usr-balance").innerText = ` ${userAmnt}`;
 }
 userAmnt = 45000;
+updateBalance();
 
 let withdrawBtn = document.getElementById("withdraw-btn");
 let getBonusBtn = document.getElementById("getbonus-btn");
